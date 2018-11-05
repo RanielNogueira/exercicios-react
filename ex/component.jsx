36b07b7525e15d2a2629119eea7b9a47836bc3a3
raniel.nogueira@live.com <rg3n17g89>
2018-11-05 08:35:43 -0200

    Commit Alteração

---
+++ ex/component.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+//Quando é apenas 1 parametro não precisa colocar entre parentese
+//Como no exemplo : (props)
+export default props => (
+     <h1>{props.value}</h1>
+)
